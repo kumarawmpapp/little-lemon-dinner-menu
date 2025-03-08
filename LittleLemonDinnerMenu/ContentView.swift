a2b449495292e373b8ed5297e1ee56f98e69b5b4
Pradeep Kumara <kumarawmpapp@gmail.com>
2025-03-08 19:56:54 +0530

    Initial Commit

---
+++ LittleLemonDinnerMenu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LittleLemonDinnerMenu
+//
+//  Created by Pradeep Kumara on 3/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LittleLemonDinnerMenu/LittleLemonDinnerMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonDinnerMenuApp.swift
+//  LittleLemonDinnerMenu
+//
+//  Created by Pradeep Kumara on 3/8/25.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonDinnerMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
